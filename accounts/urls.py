6e0ec5a463f6f43de1578e35313b820c5676b20b
LeeCarty <lichenglin2014.com@outlook.com>
2018-03-23 19:02:32 +0800

    登录、注册、忘记密码功能及前端页面展示（部分实现）！！前后端交互方式确定！！

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,8 @@ from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
-    url(r'^login$', csrf_exempt(views.login), name='login'),
+    url(r'^$', csrf_exempt(views.login), name='login'),
     url(r'^register$', csrf_exempt(views.register), name='register'),
+    url(r'^password_modify$', csrf_exempt(views.password_modify), name='password_modify'),
+    url(r'^email_confirm$', csrf_exempt(views.email_confirm), name='email_confirm'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -10,38 +10,85 @@ from .models import Accounts
 def login(request):
     request.encoding = 'utf-8'
     context = {}
-    error_msg = ""
+    error_msg = ''
     if request.method == "POST":
         email = request.POST['account_email']
         psd = request.POST['account_password']
-        account_id = Accounts.objects.get(email=email)
-        if account_id and account_id.password == psd:
-            # 跳转到系统主页
-            context = {'account': account_id}
-            return render(request, 'base.html', context=context)
-        else:
-            # 用户密码不匹配
-            error_msg = '用户邮箱或密码错误！'
-    context = {'error_msg': error_msg}
-    return render(request, 'accounts/login.html', context=context)
+        try:
+            account_id = Accounts.objects.get(email=email)
+            if account_id and account_id.password == psd:
+                # 跳转到系统主页
+                context['account'] = account_id
+                return render(request, 'base.html', context=context)
+            else:
+                # 用户密码不匹配
+                error_msg = '用户邮箱或密码错误！'
+        except Accounts.DoesNotExist:
+            error_msg = '用户不存在！请注册后重试！'
+
+    context['error_msg'] = error_msg
+    return render(request, 'accounts/index2.html', context=context)
 
 
 # 处理用户注册请求
 def register(request):
-    content = {}
-    error_msg = ""
+    context = {}
+    error_msg = ''
     if request.method == 'POST':
         user_name = request.POST['account_name']
         email = request.POST['account_email']
         psd = request.POST['account_password']
-
+        psd2 = request.POST['account_password2']
         try:
-            account_id2 = Accounts.objects.get(email=email)
+            Accounts.objects.get(email=email)
             # 如果没有查找到数据，会报 DoesNotExist 错误
             error_msg = '该邮箱已被使用！请重新输入！'
         except Accounts.DoesNotExist:
-            Accounts(name=user_name, email=email, password=psd).save()
-            return render(request, 'accounts/login.html', context=content)
+            # 判断两次密码是否一致
+            if psd == psd2:
+                Accounts(name=user_name, email=email, password=psd).save()
+                return render(request, 'accounts/index2.html', context=context)
+            else:
+                error_msg = '两次输入的密码不匹配！'
 
-    content = {'error_msg': error_msg}
-    return render(request, 'accounts/register.html', context=content)
+    context['error_msg'] = error_msg
+    return render(request, 'accounts/sign-up2.html', context=context)
+
+
+# 找回密码前邮箱确认请求处理
+def email_confirm(request):
+    context = {}
+    error_msg = ''
+    if request.method == 'POST':
+        email = request.POST['account_email']
+        try:
+            account_id = Accounts.objects.get(email=email)
+            context['account_email'] = account_id.email
+            return render(request, 'accounts/passwordmodify.html', context=context)
+        except Accounts.DoesNotExist:
+            error_msg = '该邮箱未注册！请确认后重试！'
+
+    context['error_msg'] = error_msg
+    return render(request, 'accounts/forgot2.html', context=context)
+
+
+# 密码修改请求处理
+def password_modify(request):
+    context = {}
+    error_msg = ''
+    if request.method == 'POST':
+        email = request.POST['account_email']
+        psd = request.POST['account_password']
+        psd2 = request.POST['account_password2']
+        account_id = Accounts.objects.get(email=email)
+        # 判断两次密码是否一致
+        if psd == psd2:
+            account_id.password = psd
+            account_id.save()
+            context['account'] = account_id
+            return render(request, 'accounts/login.html', context=context)
+        else:
+            error_msg = '两次输入的密码不一致！请重新输入！'
+
+    context['error_msg'] = error_msg
+    return render(request, 'accounts/passwordmodify.html', context=context)
